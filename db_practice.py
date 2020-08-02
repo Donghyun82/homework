af27ddfe07478f0e497c091d8463c3e53d6fe8e6
Donghyun82 <rubelloia@gmail.com>
2020-08-02 14:16:58 +0900

    Update db_practice.py

--- db_practice.py
+++ db_practice.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient
-​
+from pymongo import M​
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 ​
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# 아래 빈 칸('')을 채워보세요
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 ​
@@ -18,12 +16,15 @@ for tr in trs:
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
 ​
+print(rank,artist,title)
+
     doc = {
       'rank':rank,
       'title':title,
       'artist':artist,
     }
     db.donghyun.insert_one(doc)
+
 
 
 
