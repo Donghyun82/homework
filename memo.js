01ad886d1e305b4fc35f55b25d9fe4b08597d0e1
Donghyun82 <rubelloia@gmail.com>
2020-08-02 13:58:01 +0900

    Test
    Re-type some codes

---
+++ memo.js
@@ -0,0 +1,13 @@
+let cities = ['런던','뉴욕','서울','도쿄','오사카','빈']
+for (let i = 0 ; i < cities.length ; i++) {
+}
+function isCity(cities) {
+    if (city['cities']) = cities['i']){
+return true;
+} else
+{
+    return false;
+}
+
+console.log(isCity);
+
